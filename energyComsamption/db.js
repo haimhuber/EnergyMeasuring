dfacacf0779d79cabaeee16e69c45ca87838939f
ILHAHUB <haimhuber90@gmail.com>
2026-03-08 16:39:01 +0200

    updated and merged

--- energyComsamption/db.js
+++ energyComsamption/db.js
@@ -4,10 +4,10 @@ dotenv.config({ path: './.env.unified' });
 
 async function connectionToSqlDB() {
     const config = {
-        server: 'localhost\\ABB_2019',
-        user: 'abb',
-        password: '1234',
-        database: 'EnergyManagment',
+        server: process.env.DB_SERVER,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
         options: {
             encrypt: false,
             trustServerCertificate: true
