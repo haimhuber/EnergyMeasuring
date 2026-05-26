088de5654d6a3992366656ceabf01d225e440399
Haim Huber <haimhuber90@gmail.com>
2026-05-26 15:59:12 +0300

    Updated to the main branch

--- energyComsamption/energyManegmant.js
+++ energyComsamption/energyManegmant.js
@@ -2,8 +2,8 @@ const readEnergyData = require('./opcUaReadEnergyData');
 const { DateTime } = require('luxon');
 const { scheduleNextRun } = require('./scheduleCycle');
 
-// scheduleNextRun(readEnergyData.readOpcActiveEnergyTags);
-readEnergyData.readOpcActiveEnergyTags();
+scheduleNextRun(readEnergyData.readOpcActiveEnergyTags);
+// readEnergyData.readOpcActiveEnergyTags();
 
 
 
