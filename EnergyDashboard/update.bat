6b33748dea4f2beb5bbb775d46ff3a2138159a23
Haim Huber <haimhuber90@gmail.com>
2026-05-27 14:57:08 +0300

    Updated machnisam has updated

--- EnergyDashboard/update.bat
+++ EnergyDashboard/update.bat
@@ -2,28 +2,32 @@
 echo [UPDATE] Starting update process...
 
 cd C:\EnergyMeasuring
-
 echo [UPDATE] Pulling latest changes...
 git pull origin main
 
 echo [UPDATE] Installing dependencies - EnergyDashboard...
 cd C:\EnergyMeasuring\EnergyDashboard
-npm install
+call npm install
 
 echo [UPDATE] Installing dependencies - energyComsamption...
 cd C:\EnergyMeasuring\energyComsamption
-npm install
+call npm install
 
 echo [UPDATE] Building React app...
 cd C:\EnergyMeasuring\abb-energy-vite
-npm install
-npm run build
+call npm install
+call npm run build
 
 echo [UPDATE] Copying build to public...
 xcopy /E /I /Y C:\EnergyMeasuring\abb-energy-vite\dist C:\EnergyMeasuring\EnergyDashboard\public
 
-echo [UPDATE] Restarting services...
+echo [UPDATE] Restarting EnergyDataV2...
 nssm restart EnergyDataV2
-nssm restart EnergyDataDashboardV2
+
+echo [UPDATE] Waiting 5 seconds...
+timeout /t 5 /nobreak
+
+echo [UPDATE] Restarting Dashboard (this process will end)...
+start /b cmd /c "timeout /t 3 /nobreak && nssm restart EnergyDataDashboardV2"
 
 echo [UPDATE] Done!
